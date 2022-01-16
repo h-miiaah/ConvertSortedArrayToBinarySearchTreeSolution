97ea8e6d12baf1937a562a216d0e79e01d8ec824
h-miiaah <haim.miiaah5@gmail.com>
2022-01-16 18:36:55 -0500

    comments

--- ConvertSortedArrayToBinarySearchTree.js
+++ ConvertSortedArrayToBinarySearchTree.js
@@ -29,5 +29,6 @@ var treeHelper = function (nums, ll, rr) {
   - The treeHelper function takes in the sorted array, the left index, and the right index.
   - If the left index is greater than the right index, then we know that the array is empty, so we return null.
   - Otherwise, we find the middle index of the array.
-
+  - We create a new TreeNode with the value at the middle index.
+  
   */
